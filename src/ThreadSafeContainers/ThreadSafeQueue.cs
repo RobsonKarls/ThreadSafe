6785c0f390517a770fbfb84dd91aada2bdf757d3
Robison.Custodio <Robison.Custodio@RCUSTODIO-P5520>
2019-06-04 17:02:33 -0400

    first commit

---
+++ src/ThreadSafeContainers/ThreadSafeQueue.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadSafeContainers {
+    /// <summary>
+    /// Despite the name of the class, it is currently not thread-safe, and is missing use-cases as defined
+    /// in the test outline.  Some methods may also be missing basic input and output validation that should
+    /// also be taken into consideration
+    /// </summary>
+    public sealed class ThreadSafeQueue<T> {
+        private readonly Queue<T> _queue = new Queue<T>();
+        private object _lock = new object();
+
+        public IEnumerable<T> DequeueAll() {
+            var list = new List<T>();
+            lock(_lock) {
+                while (_queue.Count == 0) {
+                    Monitor.Wait(_lock);
+                }
+
+                while (_queue.Count > 0) {
+                    list.Add(_queue.Dequeue());
+                }
+
+                return list;
+            }
+        }
+
+        public int Count {
+            get {
+                lock(_lock) {
+                    return _queue.Count;
+                }
+            }
+        }
+
+        public void Enqueue(T value) {
+            lock(_lock) {
+                _queue.Enqueue(value);
+
+                Monitor.PulseAll(_lock);
+            }
+        }
+
+        public void EnqueueAll(IEnumerable<T> values) {
+            lock(_lock) {
+                foreach (T v in values) {
+                    _queue.Enqueue(v);
+                }
+
+                Monitor.PulseAll(_lock);
+            }
+        }
+
+        public Task<T> DequeueAsync() {
+            var value = _queue.Dequeue();
+            return Task.FromResult(value);
+        }
+    }
+}
---
+++ tests/ThreadSafeContainers.Tests/ThreadSafeQueueTests.cs
@@ -0,0 +1,113 @@
+﻿using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Xunit;
+
+namespace ThreadSafeContainers.Tests {
+    public sealed class ThreadSafeQueueTests {
+        [Fact]
+        public void ShouldCreateNewInstance() {
+            var queue = new ThreadSafeQueue<string>();
+            Assert.NotNull(queue);
+        }
+
+        [Fact]
+        public void ShouldDefaultToEmpty() {
+            var queue = new ThreadSafeQueue<int>();
+
+            Assert.Equal(0, queue.Count);
+            Assert.Empty(queue.DequeueAll());
+        }
+
+        [Fact]
+        public void ShouldUpdateCountWhenAddingToQueue() {
+            var queue = new ThreadSafeQueue<long>();
+
+            queue.Enqueue(5);
+            queue.Enqueue(42);
+
+            Assert.Equal(2, queue.Count);
+        }
+
+        [Fact]
+        public void ShouldDequeueAllAfterAdded() {
+            var expected = new [] {
+                "hello",
+                "world"
+            };
+
+            var queue = new ThreadSafeQueue<string>();
+
+            queue.Enqueue("hello");
+            queue.Enqueue("world");
+
+            var actual = queue.DequeueAll();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void ShouldEnqueueColletionOfItem() {
+            var expected = new [] {
+                "hello",
+                "world"
+            };
+
+            var queue = new ThreadSafeQueue<string>();
+
+            queue.EnqueueAll(expected);
+
+            var actual = queue.DequeueAll();
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void test1() {
+            this.ShouldBlockThreadUntilAnItemBecamesAvailable().Wait();
+        }
+
+        public async Task ShouldBlockThreadUntilAnItemBecamesAvailable() {
+            var expected = new [] {
+                "hello",
+                "world",
+                "TripStack"
+            };
+
+            var expectedCount = 9;
+
+            var queue = new ThreadSafeQueue<string>();
+
+            var result = await Task.Run(() => {
+                var dq = queue.DequeueAll();
+                return dq;
+            });
+
+            var loadTasks = Task.Run(() => {
+                Parallel.Invoke(
+                    () => { queue.EnqueueAll(expected); },
+                    () => { queue.EnqueueAll(expected); },
+                    () => { queue.EnqueueAll(expected); }
+                );
+            });
+
+            await loadTasks;
+
+            Assert.Equal(expectedCount, result.Count());
+
+        }
+
+        [Fact]
+        public async Task ShouldDequeueInstantlyIfValueExistsInQueue() {
+            var queue = new ThreadSafeQueue<string>();
+
+            queue.Enqueue("hello");
+            queue.Enqueue("world");
+
+            var actual = await queue.DequeueAsync();
+
+            Assert.Equal("hello", actual);
+        }
+    }
+}
